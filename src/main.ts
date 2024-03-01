9cd74c7b5dfffbeff76c969c4897021dd271003e
anton_work <aremenets1@gmail.com>
2024-03-01 13:28:50 +0500

    add ConfigService

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const PORT: number = parseInt(process.env.PORT)
+  await app.listen(PORT, () => console.log(`Server ready at port ${PORT}`))
 }
 
 bootstrap()
