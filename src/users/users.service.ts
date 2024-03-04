5f72b590c1aabfedbdcd1bd1ebb1de8179f35c04
anton_work <aremenets1@gmail.com>
2024-03-04 13:48:42 +0500

    ...

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../services/prisma/prisma.service'
 import { CreateUserDto } from './dto/create.dto'
 import * as bcrypt from 'bcrypt'
+import { User } from './user.entity'
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async registerNewUser(dto: CreateUserDto) {
-    /*
-     * Регистрация нового пользователя
-     * 1. Проверяем есть ли такой пользователь в базе, если есть выкидиваем ошибку
-     * 2. Валидируем данные
-     * 3. Сохраняем пользователя в базе данных
-     * */
+  // Регистрация нового пользователя
+  async registerNewUser(dto: CreateUserDto): Promise<Partial<User>> {
+    // 1. Проверяем есть ли такой пользователь в базе, если есть выкидиваем ошибку
+    // 2. Валидируем данные
+    // 3. Сохраняем пользователя в базе данных
+    // 4. Выдаем акцесс токен
     const candidate = await this.prisma.user.findFirst({
       where: { email: dto.email },
     })
@@ -29,6 +29,13 @@ export class UsersService {
     })
   }
 
+  // Логин
+  async loginUser() {}
+
+  // Логаут
+  async logoutUser() {}
+
+  // Хеширование пароля
   async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 10)
   }
